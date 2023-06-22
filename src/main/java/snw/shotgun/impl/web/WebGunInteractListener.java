73e559220f6b8116c0e11f5c658b4bd21385eb3e
ZX夏夜之风 <snwcreations@qq.com>
2023-06-22 10:02:37 +0800

    The web gun only works once

--- src/main/java/snw/shotgun/impl/web/WebGunInteractListener.java
+++ src/main/java/snw/shotgun/impl/web/WebGunInteractListener.java
@@ -1,5 +1,7 @@
 package snw.shotgun.impl.web;
 
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -22,6 +24,10 @@ public final class WebGunInteractListener implements Listener {
                 final ItemStack item = event.getItem();
                 if (WebBullet.ITEM.isSimilar(item)) {
                     new WebBullet(event.getPlayer(), plugin).start(this.plugin);
+                    item.setAmount(0);
+                    event.getPlayer().playSound(
+                            event.getPlayer().getLocation(), Sound.ENTITY_ITEM_BREAK, SoundCategory.MASTER,
+                            1, 1);
                 }
             }
         }
